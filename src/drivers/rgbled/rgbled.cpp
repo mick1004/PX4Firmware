35665032adccf130cadbe049abccaf558552b4cb
Lorenz Meier <lm@inf.ethz.ch>
2015-02-13 09:10:32 +0100

    RGB led: Code style and comment on bus speed

--- src/drivers/rgbled/rgbled.cpp
+++ src/drivers/rgbled/rgbled.cpp
@@ -1,8 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2012, 2013 PX4 Development Team. All rights reserved.
- *   Author: Julian Oes <joes@student.ethz.ch>
- *           Anton Babushkin <anton.babushkin@me.com>
+ *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,6 +36,8 @@
  *
  * Driver for the onboard RGB LED controller (TCA62724FMG) connected via I2C.
  *
+ * @author Julian Oes <julian@px4.io>
+ * @author Anton Babushkin <anton.babushkin@me.com>
  */
 
 #include <nuttx/config.h>
